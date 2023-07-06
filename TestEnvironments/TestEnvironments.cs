f8e13dba8989cf299d23e6b82338889dc1163c12
Kushang Gajjar <g.kushang@gmail.com>
2023-07-06 17:58:38 -0400

    Update TestEnvironments.cs

--- TestEnvironments/TestEnvironments.cs
+++ TestEnvironments/TestEnvironments.cs
@@ -21,34 +21,29 @@ namespace SpecFlowFramework.TestEnvironments
             string testDirectoryWithoutBin = testDirectory.Substring(0, testDirectory.Length - binDirectory.Length);
 
             Console.WriteLine("Test Directory: " + testDirectoryWithoutBin);
-
-
         
             string delimiter = "bin";
-
-            // Split the string using the delimiter
+            
             string[] _parts = testDirectory.Split(new[] { delimiter }, StringSplitOptions.None);
 
-            // Get the first part
             string firstPart = _parts[0];
 
             Console.WriteLine(firstPart);
              string filePath = firstPart + "TestEnvironments/staging_1.txt"; // Replace with the actual file path
 
-                // Read all lines from the file
+                
                 string[] lines = File.ReadAllLines(filePath);
 
-                // Iterate over each line and set it as an environment variable
+                
                 foreach (string line in lines)
                 {
-                    string[] parts = line.Split("="); // Assuming the lines are in "key=value" format
+                    string[] parts = line.Split("="); 
 
                     if (parts.Length == 2)
                     {
                         string key = parts[0].Trim();
                         string value = parts[1].Trim();
 
-                        // Set the environment variable
                         Environment.SetEnvironmentVariable(key, value);
                         Console.WriteLine("ENV: " + key + "=" + value);
                     }
